3a3a48bc23f9654695fb122e22ab2a7bf718f1b1
Kyle Surowiec <kylesurowiec@me.com>
2023-11-12 16:23:47 -0600

    test 3

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod parser;
 use std::io::{Read, Write};
 use std::net::TcpListener;
 
-fn main() {
+use anyhow::Result;
+
+fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:4221").expect("Failed to bind TcpListener");
 
     for stream in listener.incoming() {
@@ -11,21 +13,22 @@ fn main() {
             Ok(mut stream) => {
                 println!("Accepted new connection");
 
-                let mut buffer = vec![];
+                let mut buffer = [0; 1024];
                 stream.read(&mut buffer).unwrap();
 
-                let buffer_as_string = String::from_utf8_lossy(&buffer);
-                println!("{:#?}", buffer_as_string);
+                let raw_http = parser::RawHttpRequest::new(&buffer)?;
 
-                //let _raw = parser::RawHttpRequest::new(buffer.to_ascii_lowercase());
-
-                println!("made it here");
-
-                let _ = stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
+                if raw_http.path == "/" {
+                    let _ = stream.write(b"HTTP/1.1 200 OK\r\n\r\n");
+                } else {
+                    let _ = stream.write(b"HTTP/1.1 404 NOT FOUND\r\n\r\n");
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,13 @@
+use anyhow::Result;
+
+pub enum HttpMethod {
+    Get,
+    Patch,
+    Post,
+    Put,
+    Delete,
+}
+
 pub struct Header {
     pub key: String,
     pub value: String,
@@ -6,23 +16,59 @@ pub struct Header {
 pub struct RawHttpRequest {
     pub start_line: String,
     pub path: String,
-    pub http_method: String,
-    pub http_version: String,
+    pub method: HttpMethod,
+    pub version: String,
     pub headers: Vec<Header>,
 }
 
 impl RawHttpRequest {
-    pub fn new(_req: Vec<u8>) -> Self {
+    pub fn new(buffer: &[u8]) -> Result<Self> {
+        let buf_as_string = String::from_utf8_lossy(buffer).to_string();
+
+        let start_line = RawHttpRequest::parse_start_line(buf_as_string)?;
+        let path = RawHttpRequest::parse_path(start_line.clone())?;
+        let version = RawHttpRequest::parse_version(start_line.clone())?;
+
+        println!("{:#?}", version);
+
         let header = Header {
             key: "test".to_string(),
             value: "test".to_string(),
         };
-        Self {
-            start_line: "".to_string(),
-            path: "".to_string(),
-            http_method: "".to_string(),
-            http_version: "".to_string(),
+
+        Ok(Self {
+            start_line,
+            path,
+            method: HttpMethod::Get,
+            version,
             headers: vec![header],
+        })
+    }
+
+    fn parse_start_line(raw_req: String) -> Result<String> {
+        let start_line = raw_req.split("\r\n").nth(0);
+
+        match start_line {
+            Some(start_line) => Ok(start_line.to_string()),
+            None => Err(anyhow::Error::msg("Failed to parse start line")),
+        }
+    }
+
+    fn parse_path(start_line: String) -> Result<String> {
+        let path = start_line.split_whitespace().nth(1);
+
+        match path {
+            Some(path) => Ok(path.to_string()),
+            None => Err(anyhow::Error::msg("Failed to parse path")),
+        }
+    }
+
+    fn parse_version(start_line: String) -> Result<String> {
+        let version = start_line.split_whitespace().nth(2);
+
+        match version {
+            Some(version) => Ok(version.to_string()),
+            None => Err(anyhow::Error::msg("Failed to parse path")),
         }
     }
 }
