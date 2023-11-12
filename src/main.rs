f1bd780d78989c793693d678d32127ec65409ba8
Kyle Surowiec <kylesurowiec@me.com>
2023-11-12 02:50:46 -0600

    test 3

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:4221").expect("Failed to bind TcpListener");
 
     for stream in listener.incoming() {
         match stream {
@@ -14,7 +14,7 @@ fn main() {
                 let mut buf = vec![];
                 let _message = stream.read(&mut buf);
 
-                println!("{:#?}", buf.to_ascii_lowercase());
+                println!("{:#?}", buf);
 
                 let _raw = parser::RawHttpRequest::new(buf.to_ascii_lowercase());
                 println!("made it here");
