6bf65a3bbb166f9dc0ae5b8770a9d23f8d9272ec
Kyle Surowiec <kylesurowiec@me.com>
2023-11-13 02:16:56 -0600

    part 4

---
+++ src/http_headers.rs
@@ -0,0 +1,6 @@
+pub const CONTENT_TYPE: &str = "Content-Type: {}";
+pub const CONTENT_TYPE_HTML: &str = "text/html";
+pub const CONTENT_TYPE_JSON: &str = "text/json";
+pub const CONTENT_TYPE_TEXT: &str = "text/plain";
+
+pub const CONTENT_LENGTH: &str = "Content-Length:";
